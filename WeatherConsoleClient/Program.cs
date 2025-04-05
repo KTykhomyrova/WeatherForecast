260bb7a47dada880fc1f4037523bc8cb3db781c0
Kateryna <tykhomyrovak@gmail.com>
2025-04-05 12:19:40 +0200

    Adding new projects

---
+++ WeatherConsoleClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WeatherConsoleClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Создание HttpClient
+            using (var client = new HttpClient())
+            {
+                // Установка базового адреса
+                client.BaseAddress = new Uri("https://localhost:7233/WeatherForecast");
+
+                // Отправка GET запроса для получения данных о погоде
+                try
+                {
+                    var response = await client.GetAsync(""); // Пустой путь, так как контроллер настроен на /weatherforecast
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var weatherData = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("Weather data: ");
+                        Console.WriteLine(weatherData);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка при получении данных о погоде.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
